51b9d70f6398b2cb1b347e1b77958f114e75f4b4
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-02 15:58:54 +0200

    Read packets

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 extern "C" {
     #include <libavformat/avformat.h>
 }
+#include <iostream>
 // https://ffmpeg.org/
 // https://ffmpeg.org/ffmpeg.html
 
@@ -9,12 +10,20 @@ int main() {
     // https://ffmpeg.org/ffmpeg-formats.html
     // https://ffmpeg.org/doxygen/trunk/group__libavf.html
     // https://ffmpeg.org/doxygen/trunk/group__lavf__decoding.html#gac05d61a2b492ae3985c658f34622c19d
-    const char    *url = "file:in.mp3";
-    AVFormatContext *s = NULL;
-    int ret = avformat_open_input(&s, url, NULL, NULL);
+    const char    *url = "file:test.mp4";
+    AVFormatContext *av_format_context = NULL;
+    int ret = avformat_open_input(&av_format_context, url, NULL, NULL);
+    // TODO avformat_find_stream_info()
     if (ret < 0)
         abort();
 
+    AVPacket* av_packet = av_packet_alloc();
+    while (av_read_frame(av_format_context, av_packet) == 0) {
+        std::cout << "Read packet" << std::endl;
+    }
+
+
+    avformat_close_input(&av_format_context);
 
     // then streamcopy (or decode for partial keyframe shit)
     // https://ffmpeg.org/ffmpeg-codecs.html
